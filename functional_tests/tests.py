818b9a31f1ca42da47c827b07f63f57889fdd940
OctaveInfinity <e.makoviak@gmail.com>
2019-12-22 17:15:16 +0200

    New URL, view and template to display list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
 from django.test import LiveServerTestCase 
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
-from selenium.common.exceptions import WebDriverException
 
 MAX_WAIT = 10 
 
@@ -11,8 +11,10 @@ class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
+
     def tearDown(self):
         self.browser.quit()
+
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
@@ -27,6 +29,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5) 
  
+
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -72,6 +75,7 @@ class NewVisitorTest(LiveServerTestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep 
+
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
