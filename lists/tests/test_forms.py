14e3eebd5899ef2bb1a602c984dc3f9d9c44bba1
OctaveInfinity <e.makoviak@gmail.com>
2020-01-18 15:57:11 +0200

    use new form in home_page, simplify tests. NB breaks stuff

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -8,11 +8,6 @@ from lists.forms import ItemForm
 class ItemFormTest(TestCase):
 
 
-    # def test_form_renders_item_text_input(self):
-    #     form = ItemForm()
-    #     self.fail(form.as_p())
-        
-
     def test_form_item_input_has_placeholder_and_css_classes(self):
         form = ItemForm()
         self.assertIn('placeholder="Enter a to-do item"', form.as_p())
@@ -21,11 +16,6 @@ class ItemFormTest(TestCase):
 
     def test_form_validation_for_blank_items(self):
         form = ItemForm(data={'text': ''})
-        # form.save()
         self.assertFalse(form.is_valid())
-        # self.assertEqual(
-        #     form.errors['text'],
-        #     ["You can't have an empty list item"]
-        # )
         self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
 
