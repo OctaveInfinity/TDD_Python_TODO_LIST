95c86f74644ea157a69b98c734b4a9891fa4ff47
OctaveInfinity <e.makoviak@gmail.com>
2020-01-25 22:24:50 +0200

    Some Update

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -1,7 +1,6 @@
 from fabric.api import run
 from fabric.context_managers import settings, shell_env
 
-host = "auto.octave-infinity.com"
 
 def _get_manage_dot_py(host):
     return f'~/sites/{host}/virtualenv/bin/python ~/sites/{host}/manage.py'
